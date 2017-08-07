7d1755b0520d93ffc2dee8204a7d8a21bf461564
Janis Giovani Tan <janis_giovani@ymail.com>
2017-08-07 13:51:12 +0700

    Add variable playgound area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+/*
+var person={
+    name:'Andrew',
+    age:21
+};
+
+function updatePerson(obj) {
+    // obj = {
+    //     name:'Andrew',
+    //     age:24
+    // };
+    obj.age =24;
+}
+
+updatePerson(person);
+console.log(person);
+
+*/
+
+//Array example
+var grades = [15,37]
+
+
+function addGrade(gradesArr) {
+    gradesArr.push(53);
+    debugger;
+}
+
+addGrade(grades);
+console.log(grades);
