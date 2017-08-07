ab18d813f3ed29b442ea792abad9958ab8b2e651
Janis Giovani Tan <janis_giovani@ymail.com>
2017-08-07 10:17:27 +0700

    Add host for todos

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+/*var todos = [{
     id: 1,
     description: 'Meet om for lunch',
     competed: false
@@ -13,8 +19,7 @@ var todos = [{
     id: 3,
     description: 'Work!',
     competed: true
-}
-];
+}];*/
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -24,23 +29,34 @@ app.get('/todos', function(req,res) {
     res.json(todos);
 });
 
-app.get('/todos/:id', function(req,res) {
+app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
 
-    todos.forEach(function (todo){
-        if(todoId === todo.id) {
+    todos.forEach(function (todo) {
+        if (todoId === todo.id) {
             matchedTodo = todo;
         }
     });
 
-    if(matchedTodo) {
-        res.json(matchedTodo)
-    }else {
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
         res.status(404).send();
     }
+});
 
-    res.send('Asking for todo with id of ' + req.params.id);
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    // add id field
+    body.id = todoNextId++;
+
+    // push body into array
+    todos.push(body);
+
+    res.json(body);
 });
 
 
